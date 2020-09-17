753d6971c4f45ac16107cf234ff24db12951b833
Mishabuzov <mikhaijl7@gmail.com>
2020-09-17 20:49:28 +0300

    implemented simple JobIntentService, which returns message

---
+++ HomeWork_1/LocalBroadcastReceiverHW/app/src/main/java/ru/home/localbroadcastreceiverhw/service/ExtractContactsService.kt
@@ -0,0 +1,33 @@
+package ru.home.localbroadcastreceiverhw.service
+
+import android.content.Intent
+import android.util.Log
+import androidx.core.app.JobIntentService
+import java.util.concurrent.TimeUnit
+
+class ExtractContactsService : JobIntentService() {
+
+    companion object {
+        internal const val ACTION_CONTACTS_SERVICE =
+            "ru.home.localbroadcastreceiverhw.service.RESPONSE"
+        internal const val EXTRA_KEY_OUT = "EXTRA_OUT"
+
+        internal const val WORK_TAG = "SERVICE_WORK"
+    }
+
+    override fun onHandleWork(intent: Intent) {
+        val label = intent.getStringExtra(WORK_TAG)
+        Log.d("ExtractContactsService", "onHandleWork started: working with $label")
+        TimeUnit.SECONDS.sleep(5)
+        Log.d("ExtractContactsService", "work emulation is done")
+
+        val serviceOutput = "This is a message from the service"
+        sendBroadcast(
+            Intent()
+                .setAction(ACTION_CONTACTS_SERVICE)
+                .addCategory(Intent.CATEGORY_DEFAULT)
+                .putExtra(EXTRA_KEY_OUT, serviceOutput)
+        )
+    }
+
+}
