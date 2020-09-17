a1776df65e3e837587d9423aa77295f786aa4609
Mishabuzov <mikhaijl7@gmail.com>
2020-09-17 20:53:19 +0300

    implemented activity that displays message from service passed through ServiceActivity

---
+++ HomeWork_1/LocalBroadcastReceiverHW/app/src/main/java/ru/home/localbroadcastreceiverhw/main/MainActivity.kt
@@ -0,0 +1,25 @@
+package ru.home.localbroadcastreceiverhw.main
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.home.localbroadcastreceiverhw.R
+import ru.home.localbroadcastreceiverhw.service.ServiceActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ServiceActivity.startForContacts(this)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            val serviceMessage = data?.getStringExtra(ServiceActivity.SERVICE_RESULT_KEY)
+            message_text_view.text = serviceMessage
+        }
+    }
+}
