2c0722cb5c995e5a04516dbc511db9cff6af9596
Mishabuzov <mikhaijl7@gmail.com>
2020-09-19 00:47:06 +0300

    implemented Parcelable

--- HomeWork_1/LocalBroadcastReceiverHW/app/src/main/java/ru/home/localbroadcastreceiverhw/Contact.kt
+++ HomeWork_1/LocalBroadcastReceiverHW/app/src/main/java/ru/home/localbroadcastreceiverhw/Contact.kt
@@ -1,8 +1,33 @@
 package ru.home.localbroadcastreceiverhw
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Contact(
+    val id: String,
     val name: String,
-    val surname: String,
-    val email: String,
-    val phoneNumbers: List<String>
-)
+//    val surname: String,
+//    val email: String,
+    val phoneNumbers: List<String>?
+) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.createStringArrayList()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(name)
+        parcel.writeStringList(phoneNumbers)
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object CREATOR : Parcelable.Creator<Contact> {
+        override fun createFromParcel(parcel: Parcel): Contact = Contact(parcel)
+
+        override fun newArray(size: Int): Array<Contact?> = arrayOfNulls(size)
+    }
+}
