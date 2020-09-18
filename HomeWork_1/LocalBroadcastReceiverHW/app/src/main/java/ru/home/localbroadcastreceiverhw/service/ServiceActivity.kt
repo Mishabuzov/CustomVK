a903c6a4a691f0613f5283e0bec340b2778e6dd9
Mishabuzov <mikhaijl7@gmail.com>
2020-09-19 00:44:53 +0300

    added permissions processing for reading contacts

--- HomeWork_1/LocalBroadcastReceiverHW/app/src/main/java/ru/home/localbroadcastreceiverhw/service/ServiceActivity.kt
+++ HomeWork_1/LocalBroadcastReceiverHW/app/src/main/java/ru/home/localbroadcastreceiverhw/service/ServiceActivity.kt
@@ -1,17 +1,24 @@
 package ru.home.localbroadcastreceiverhw.service
 
+import android.Manifest
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.view.View.GONE
 import androidx.appcompat.app.AppCompatActivity
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import kotlinx.android.synthetic.main.activity_service.*
+import ru.home.localbroadcastreceiverhw.R
 
 class ServiceActivity : AppCompatActivity() {
 
     companion object {
         private const val REQUEST_CODE_CONTACTS = 1
+        private const val PERMISSIONS_REQUEST_READ_CONTACTS = 100
         internal const val SERVICE_RESULT_KEY = "service result message"
 
         fun startForContacts(activity: AppCompatActivity) = activity.startActivityForResult(
@@ -29,7 +36,19 @@ class ServiceActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startContactsService(this)
+        setContentView(R.layout.activity_service)
+        checkContactsPermissionAndStartService()
+    }
+
+    private fun checkContactsPermissionAndStartService() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+            && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(
+                arrayOf(Manifest.permission.READ_CONTACTS),
+                PERMISSIONS_REQUEST_READ_CONTACTS
+            )
+        } else startContactsService(this)
     }
 
     override fun onStart() {
@@ -50,10 +69,28 @@ class ServiceActivity : AppCompatActivity() {
                 IntentFilter(ExtractContactsService.ACTION_CONTACTS_SERVICE)
             )
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startContactsService(this)
+            } else {
+                notification_text_view.text =
+                    getString(R.string.no_contacts_permission_notification)
+                progress_loading.visibility = GONE
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
     inner class ContactsBroadcastReceiver : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
-            val serviceResult = intent?.getStringExtra(ExtractContactsService.EXTRA_KEY_OUT)
-            setResult(RESULT_OK, Intent().putExtra(SERVICE_RESULT_KEY, serviceResult))
+//            val serviceResult = intent?.getStringExtra(ExtractContactsService.EXTRA_KEY_OUT)
+//            setResult(RESULT_OK, Intent().putExtra(SERVICE_RESULT_KEY, serviceResult))
+            setResult(RESULT_OK, intent)
             finish()
         }
     }
