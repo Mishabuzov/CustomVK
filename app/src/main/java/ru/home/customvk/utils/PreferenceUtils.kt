34086616bd967e283627a71456bdf71aef03ce0d
Mishabuzov <mikhaijl7@gmail.com>
2020-11-27 14:11:56 +0300

    implemented saving recycler position when changing fragments/screen rotation.

--- app/src/main/java/ru/home/customvk/utils/PreferenceUtils.kt
+++ app/src/main/java/ru/home/customvk/utils/PreferenceUtils.kt
@@ -1,23 +1,49 @@
 package ru.home.customvk.utils
 
-import android.content.Context
 import android.content.Context.MODE_PRIVATE
+import ru.home.customvk.VkApplication
 
 object PreferenceUtils {
 
     private const val SHARED_PREFERENCES_NAME = "SHARED_PREFERENCES"
     private const val ACCESS_TOKEN_KEY = "ACCESS_TOKEN"
+    private const val POSTS_RECYCLER_KEY = "POSTS_RECYCLER"
+    private const val FAVORITES_RECYCLER_KEY = "FAVORITES_RECYCLER"
 
-    fun saveToken(context: Context, accessToken: String) = context.getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
-        .edit()
-        .putString(ACCESS_TOKEN_KEY, accessToken)
-        .apply()
+    private fun getDefaultSharedPreferences() = VkApplication.instance.getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
 
-    fun getToken(context: Context) = context.getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
-        .getString(ACCESS_TOKEN_KEY, "")
+    fun saveToken(accessToken: String) {
+        getDefaultSharedPreferences()
+            .edit()
+            .putString(ACCESS_TOKEN_KEY, accessToken)
+            .apply()
+    }
 
-    fun removeToken(context: Context) = context.getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
-        .edit()
-        .remove(ACCESS_TOKEN_KEY)
-        .apply()
+    fun getToken() = getDefaultSharedPreferences().getString(ACCESS_TOKEN_KEY, "")
+
+    fun removeToken() {
+        getDefaultSharedPreferences()
+            .edit()
+            .remove(ACCESS_TOKEN_KEY)
+            .apply()
+    }
+
+    private fun getKeyForSavingAdapterPosition(isFavoritesFragment: Boolean): String {
+        return if (isFavoritesFragment) {
+            FAVORITES_RECYCLER_KEY
+        } else {
+            POSTS_RECYCLER_KEY
+        }
+    }
+
+    fun saveRecyclerPosition(position: Int, isFavoritesFragment: Boolean) {
+        getDefaultSharedPreferences()
+            .edit()
+            .putInt(getKeyForSavingAdapterPosition(isFavoritesFragment), position)
+            .apply()
+    }
+
+    fun getRecyclerPosition(isFavoritesFragment: Boolean = false): Int {
+        return getDefaultSharedPreferences().getInt(getKeyForSavingAdapterPosition(isFavoritesFragment), 0)
+    }
 }
