b5adfde2927e1145a0ef4efff576bf767deddbdf
Mishabuzov <mikhaijl7@gmail.com>
2020-12-04 03:35:09 +0300

    fixed bug with no default state for posts without images,

--- app/src/main/java/ru/home/customvk/presentation/posts_screen/adapter/PostsHolder.kt
+++ app/src/main/java/ru/home/customvk/presentation/posts_screen/adapter/PostsHolder.kt
@@ -8,6 +8,7 @@ import android.view.View.VISIBLE
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
@@ -22,9 +23,12 @@ open class TextPostHolder(itemView: View, val onLikeAction: (Int) -> Unit) : Rec
         private const val DELAY_FOR_DISABLING_LIKE_AFTER_CLICK_MS = 500L
     }
 
-    private fun ImageView.loadImage(imageUrl: String) = Glide.with(this)
-        .load(imageUrl)
-        .into(this)
+    private fun ImageView.setupImage(imageUrl: String) {
+        this.clearImageView()
+        Glide.with(this)
+            .load(imageUrl)
+            .into(this)
+    }
 
     private fun TextView.bindTextContent(textContent: String) =
         if (textContent.isBlank()) {
@@ -58,7 +62,7 @@ open class TextPostHolder(itemView: View, val onLikeAction: (Int) -> Unit) : Rec
     /**
      * Binding of comments/sharing button
      */
-    private fun Button.bindAdditionalButtons(count: Int) =
+    private fun Button.bindAdditionalButtons(count: Int) {
         if (count > 0) {
             text = count.toString()
             compoundDrawablePadding = resources.getDimensionPixelSize(R.dimen.post_action_buttons_drawable_padding)
@@ -66,21 +70,29 @@ open class TextPostHolder(itemView: View, val onLikeAction: (Int) -> Unit) : Rec
             text = ""
             compoundDrawablePadding = 0
         }
+        setOnClickListener(null)
+    }
 
-    open fun bind(post: Post) = with(itemView) {
-        // bind header
-        avatarImageView.loadImage(post.source.sourceIconUrl)
-        groupNameTextView.text = post.source.sourceName
-        timeTextView.text = post.readablePublicationDate
+    open fun bind(post: Post) {
+        with(itemView) {
+            // bind header
+            avatarImageView.setupImage(post.source.sourceIconUrl)
+            groupNameTextView.text = post.source.sourceName
+            timeTextView.text = post.readablePublicationDate
 
-        mainTextView.bindTextContent(post.text)
-        // bind footer
-        likeButton.bindLikes(post)
-        commentButton.bindAdditionalButtons(post.commentsCount)
-        shareButton.bindAdditionalButtons(post.sharesCount)
-        viewingTextView.bindTextContent(post.viewings.toString())
+            mainTextView.bindTextContent(post.text)
+            // bind footer
+            likeButton.bindLikes(post)
+            commentButton.bindAdditionalButtons(post.commentsCount)
+            shareButton.bindAdditionalButtons(post.sharesCount)
+            viewingTextView.bindTextContent(post.viewings.toString())
+
+            postImageView.isVisible = false
+        }
     }
 }
+
+private fun ImageView.clearImageView() = Glide.with(this).clear(this)
 
 class ImagePostHolder(
     itemView: View,
@@ -88,26 +100,28 @@ class ImagePostHolder(
     onLikeListener: (Int) -> Unit
 ) : TextPostHolder(itemView, onLikeListener) {
 
-    private fun ImageView.loadImageAndConfigureSharing(imageUrl: String) = Glide.with(this)
-        .asBitmap()
-        .load(imageUrl)
-        .into(object : CustomTarget<Bitmap>() {
-            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-                setImageBitmap(resource)
-                itemView.shareButton.setOnClickListener { onShareAction(resource, imageUrl) }
-            }
+    private fun ImageView.setupImageAndConfigureSharing(imageUrl: String) {
+        this.clearImageView()
+        Glide.with(this)
+            .asBitmap()
+            .load(imageUrl)
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    setImageBitmap(resource)
+                    postImageView.isVisible = true
+                    itemView.shareButton.setOnClickListener { onShareAction(resource, imageUrl) }
+                }
 
-            override fun onLoadCleared(placeholder: Drawable?) {}
-        })
+                override fun onLoadCleared(placeholder: Drawable?) = Unit
+            })
+    }
 
     override fun bind(post: Post) {
         super.bind(post)
         // bind picture
         with(itemView.postImageView) {
             if (post.pictureUrl.isNotBlank()) {
-                loadImageAndConfigureSharing(post.pictureUrl)
-            } else {
-                setImageResource(0)
+                setupImageAndConfigureSharing(post.pictureUrl)
             }
         }
     }
