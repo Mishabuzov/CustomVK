110fb32452430d8bbccb882c44b91854a109b101
Mishabuzov <mikhaijl7@gmail.com>
2020-12-04 03:15:45 +0300

    re-implemented login screen: added custom markup instead of dialogs, removed redundant token expiration handler.

--- app/src/main/java/ru/home/customvk/presentation/LoginActivity.kt
+++ app/src/main/java/ru/home/customvk/presentation/LoginActivity.kt
@@ -2,73 +2,71 @@ package ru.home.customvk.presentation
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.annotation.StringRes
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.vk.api.sdk.VK
-import com.vk.api.sdk.VKTokenExpiredHandler
 import com.vk.api.sdk.auth.VKAccessToken
 import com.vk.api.sdk.auth.VKAuthCallback
 import com.vk.api.sdk.auth.VKScope
+import kotlinx.android.synthetic.main.activity_login.*
 import ru.home.customvk.R
-import ru.home.customvk.data.api.ApiFactory
+import ru.home.customvk.VkApplication
 import ru.home.customvk.presentation.posts_screen.PostsActivity
-import ru.home.customvk.utils.PreferenceUtils
+import ru.home.customvk.utils.PreferencesUtils
+import javax.inject.Inject
 
 class LoginActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var preferencesUtils: PreferencesUtils
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val accessToken = PreferenceUtils.getToken()
-        if (accessToken.isNullOrEmpty()) {
-            VK.login(this, arrayListOf(VKScope.WALL, VKScope.FRIENDS, VKScope.OFFLINE))
+        setContentView(R.layout.activity_login)
+        (applicationContext as VkApplication).appComponent.inject(this)
+
+        val accessToken = preferencesUtils.getToken()
+        if (accessToken.isNullOrEmpty()) {  // VKScope.OFFLINE grants endless token, just check if it was already got
+            startLogin()
         } else {
             onSuccessfulLogin(accessToken)
         }
-        VK.addTokenExpiredHandler(tokenTracker)
+
+        loginButton.setOnClickListener {
+            startLogin()
+            loginButton.isEnabled = false
+        }
     }
 
-    private val tokenTracker = object : VKTokenExpiredHandler {
-        override fun onTokenExpired() {
-            restartActivity()
-            PreferenceUtils.removeToken()
-            showNotificationDialog(R.string.token_expired_message)
-        }
+    private fun startLogin() = VK.login(this, arrayListOf(VKScope.WALL, VKScope.FRIENDS, VKScope.OFFLINE))
+
+    private fun onSuccessfulLogin(accessToken: String) {
+        preferencesUtils.accessToken = accessToken
+        startActivity(PostsActivity.createIntent(this).addClearingStackFlags())
     }
 
     private fun Intent.addClearingStackFlags() = addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
 
-    fun onSuccessfulLogin(accessToken: String) {
-        ApiFactory.accessToken = accessToken
-        startActivity(PostsActivity.createIntent(this@LoginActivity).addClearingStackFlags())
-    }
-
-    private fun restartActivity() = startActivity(Intent(this, LoginActivity::class.java).addClearingStackFlags())
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         val callback = object : VKAuthCallback {
             override fun onLogin(token: VKAccessToken) {
-                PreferenceUtils.saveToken(token.accessToken)
+                preferencesUtils.saveToken(token.accessToken)
                 onSuccessfulLogin(token.accessToken)
             }
 
-            override fun onLoginFailed(errorCode: Int) = showNotificationDialog(R.string.authorization_failing_dialog_message)
+            /**
+             * Is called when clicked "cancel" in the WebView auth screen.
+             */
+            override fun onLoginFailed(errorCode: Int) = activateLoginButton()
         }
-        if (data == null || !VK.onActivityResult(requestCode, resultCode, data, callback)) {
+        if (data == null || !VK.onActivityResult(requestCode, resultCode, data, callback)) {  // true when back button is pressed.
             super.onActivityResult(requestCode, resultCode, data)
+            activateLoginButton()
         }
     }
 
-    private fun showNotificationDialog(@StringRes messageRes: Int) {
-        AlertDialog.Builder(this, R.style.AlertDialogStyle)
-            .setTitle(R.string.authorization_failing_dialog_title)
-            .setMessage(messageRes)
-            .setPositiveButton(getString(android.R.string.ok)) { dialog, _ ->
-                dialog.cancel()
-                restartActivity()
-            }
-            .setOnDismissListener { restartActivity() }
-            .create()
-            .show()
+    private fun activateLoginButton() {
+        loginButton.isVisible = true
+        loginButton.isEnabled = true
     }
 }
