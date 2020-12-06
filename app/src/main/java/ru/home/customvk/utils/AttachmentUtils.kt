56daedaf70938c115946e18753c76f42334196eb
Mishabuzov <mikhaijl7@gmail.com>
2020-12-06 22:27:00 +0300

    cleaned methods

--- app/src/main/java/ru/home/customvk/utils/AttachmentUtils.kt
+++ app/src/main/java/ru/home/customvk/utils/AttachmentUtils.kt
@@ -10,22 +10,25 @@ object AttachmentUtils {
 
     private const val TIME_FORMAT_FOR_IMAGE_NAME = "yyyy-MM-dd_HH_mm_ss"
     private const val POSTS_MIN_IMAGE_SIZE = 250
+    private const val PHOTO_ATTACHMENT_TYPE = "photo"
 
-    internal fun generateFullImageName(imageUrl: String): String {
+    fun generateFullImageName(imageUrl: String): String {
         val imageExtension = MimeTypeMap.getFileExtensionFromUrl(imageUrl)
         return "image_${System.currentTimeMillis().convertMillisTimestampToHumanReadableDate(TIME_FORMAT_FOR_IMAGE_NAME)}.$imageExtension"
     }
 
-    internal fun getImageMimeTypeByUrl(url: String): String =
-        MimeTypeMap.getSingleton().getMimeTypeFromExtension(MimeTypeMap.getFileExtensionFromUrl(url)) ?: PostUtils.DEFAULT_IMAGE_MIME_TYPE
+    fun getImageMimeTypeByUrl(url: String): String {
+        return MimeTypeMap.getSingleton().getMimeTypeFromExtension(MimeTypeMap.getFileExtensionFromUrl(url))
+            ?: PostUtils.DEFAULT_IMAGE_MIME_TYPE
+    }
 
     /**
-     * Right now considered that all attachments have photo type. The method takes only first photo with not bad quality (if such is found,
-     * either take the first one)
+     * The method takes only first photo with not bad quality (if such is found, either takes just the first photo).
      */
-    internal fun List<Attachment>.takeFirstPhotoUrl(): String {
-        return if (isNotEmpty()) {
-            val firstPhotoVariants = get(0).photo.sizes
+    fun List<Attachment>.extractPhotoUrl(): String {
+        val photoAttachments = filter { it.type == PHOTO_ATTACHMENT_TYPE }
+        return if (photoAttachments.isNotEmpty()) {
+            val firstPhotoVariants = photoAttachments[0].photo.sizes
             firstPhotoVariants.find { it.height > POSTS_MIN_IMAGE_SIZE && it.width > POSTS_MIN_IMAGE_SIZE }?.url
                 ?: firstPhotoVariants[0].url
         } else {
@@ -33,8 +36,10 @@ object AttachmentUtils {
         }
     }
 
-    fun OutputStream.compressBitmap(bitmap: Bitmap) = use {
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+    fun OutputStream.compressBitmap(bitmap: Bitmap) {
+        use {
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+        }
     }
 
 }
